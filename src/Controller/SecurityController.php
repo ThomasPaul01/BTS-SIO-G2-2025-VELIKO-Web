3d56c7a1e6bf502dc47e565ba10cd7a1875151c5
ThomasPaul01 <thomas.paul7794@gmail.com>
2024-11-08 10:02:18 +0100

    #5 new error message for connexion fail #6 add information in politique_de_confidentialite

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,12 +15,17 @@ class SecurityController extends AbstractController
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
+
+        if ($error) {
+            $this->addFlash('error', "Erreur de connexion : l'email ou le mot de passe est incorrect.");
+        }
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/index.html.twig', [
             'last_username' => $lastUsername,
-            'error' => $error,
+
         ]);
     }
 
