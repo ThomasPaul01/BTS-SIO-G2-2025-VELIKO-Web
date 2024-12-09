e11928c4a38ec1aad3f1d3e4d08b65f266bc2dfd
ThomasPaul01 <thomas.paul7794@gmail.com>
2024-12-09 09:11:33 +0100

    #10 Redirect blocked users to a specific page and restrict access to all other pages

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,43 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // récupère l'erreur de connexion s'il y en a une
+        // recupere l'erreur de connexion s'il y en a une
         $error = $authenticationUtils->getLastAuthenticationError();
-
-
         if ($error) {
-            $this->addFlash('error', "Erreur de connexion : l'email ou le mot de passe est incorrect.");
+            $this->addFlash('error', "L'email ou le mot de passe est incorrect.");
         }
 
-        // dernier nom d'utilisateur saisi par l'utilisateur
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Vérifier si l'utilisateur est bloqué
+        if ($lastUsername) {
+            $user = $this->entityManager
+                ->getRepository(User::class)
+                ->findOneBy(['email' => $lastUsername]);
+
+            if ($user && $user->isStatut()) {
+
+                $user->setRoles(['ROLE_BLOCKED']);
+
+                $this->entityManager->flush();
+
+                $this->addFlash('error', "Vous êtes bloqué, veuillez contacter le support.");
+                return $this->redirectToRoute('app_blocked');
+            }
+        }
 
         return $this->render('security/index.html.twig', [
             'last_username' => $lastUsername,
-
         ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): Response
+    public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-
-        /*return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);*/
-
     }
-
-
 }
